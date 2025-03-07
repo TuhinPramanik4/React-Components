3d5a96a5119504ad8340a08b5ae2da5d6fa7ffa8
TuhinPramanik4 <tuhinp475@gmail.com>
2025-03-08 02:14:56 +0530

    Add hober effect

---
+++ Animated-Button/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Button from './Componenets/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button />  
+    </>
+  )
+}
+
+export default App
---
+++ Animated-Button/src/Componenets/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-zinc-900'>
+    
+    <button className="transition-colors bg-white rounded-full border-2 border-blue-500 px-6 py-2 hover:bg-blue-300">
+  Motion
+</button>
+
+    
+    </div>
+  )
+}
+
+export default Button
